e84c9644896d9d7afcb5e801c86d5c8f7de1057a
Jonathan Sifuentes <jayands2k11@ymail.com>
2012-07-05 22:42:30 -0700

    initial commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include "include.h"/* essential for proper function; ncurses, etc. are included there.*/
+#ifndef OK_EXIT
+#define OK_EXIT 0 /* since everything's kosher */
+#
+int main( int argc, char *argv[]){
+	initscr();
+	cbreak();
+	keypad(stdscr,TRUE); /* for function key availability; will have to test outside of Mac OS X*/
+	/*TODO: initialize everything here  */
+	while (1){
+		/* TODO:main menu goes here */
+		/* TODO:game logic will go here */
+		break;
+	}
+
+	endwin();  /*end curses mode*/
+	exit(OK_EXIT);
+}
